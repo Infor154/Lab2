a777758732d1a2c94b5318d937c387ee8c95526c
Omotola <omotolaakeredolu@yahoo.com>
2014-07-17 20:24:55 -0400

    Updated Version

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-
+/*<!--===============================================
+   Name   : index.php
+   Purpose: Authenticates and prints out JSON results
+   Version: 5.0
+   Notes   : 
+   Author : Omotola
+ ================================================-->*/
 
        require_once('TwitterAPIExchange.php');
              
@@ -10,10 +16,17 @@
     'consumer_key' => "5Mxg8hTW6bXmi7AXlbmBIXHg1",
     'consumer_secret' => "HevfmignG9EksUAg5IqSvVKdzBBoB4LTA1FBn7lnTsmXcn5ECx"
 );
+$keyword = $_POST['keyword'];
+
+$plus = '%20';
+$keywords = explode(" ",$keyword);
+$query = implode($plus,$keywords);
+$q = str_replace('','%3A',$query);
 
 
-$url = 'https://api.twitter.com/1.1/account/verify_credentials.json';
-$getfield = '?q=#nerd';
+
+$url = 'https://api.twitter.com/1.1/search/tweets.json';
+$getfield = '?q='. $q;
 $requestMethod = 'GET';
 
 $twitter = new TwitterAPIExchange($settings);
@@ -21,4 +34,13 @@ $response = $twitter->setGetfield($getfield)
     ->buildOauth($url, $requestMethod)
     ->performRequest();
 
-var_dump(json_decode($response));
+echo'<h3>'.$query.'</h3>';
+echo'<h3>URL for the Query</h3>';
+echo'<a href='.$url.'>' .$url.'</a>';
+echo'<hr>';
+
+
+
+$tweets = json_decode($response);
+var_dump($tweets);
+//var_dump(json_decode($response));
